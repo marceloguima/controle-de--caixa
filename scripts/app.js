6da9fa9f7d01e1db38f0fe3dde6127e32218c6d8
Marcelo guimaraes <marceloguimaraesdev@gmail.com>
2025-08-12 19:40:56 -0400

    defini data atual como padrão

--- scripts/app.js
+++ scripts/app.js
@@ -8,6 +8,8 @@ formulario.addEventListener("submit", (e) => {
     e.preventDefault();
     criaCampoServico();
     limpaCampo();
+    definirDataAtualComoPadrao()
+
 });
 
 const modal = document.querySelector(".modal-filtrar");
@@ -15,22 +17,17 @@ itensLi.forEach((botao) => {
     botao.addEventListener("click", () => {
         itensLi.forEach((btn) => btn.classList.remove("active"));
         botao.classList.add("active");
-    
-   
     });
 });
 
-
 // modal para filtrar
 const showModal = () => {
-        // remove outros modais primeiro
+    // remove outros modais primeiro
     if (relatorio.classList.contains("show-relatorio")) {
         relatorio.classList.remove("show-relatorio");
-      
     }
     modal.classList.toggle("show-modal");
 };
-
 
 const showRelatorio = () => {
     // remove outros modais primeiro
@@ -39,21 +36,18 @@ const showRelatorio = () => {
     }
 
     relatorio.classList.toggle("show-relatorio");
-   
 };
 
-const liHome = document.querySelector(".li-home")
+const liHome = document.querySelector(".li-home");
 const removeModal = () => {
     relatorio.classList.remove("show-relatorio");
     modal.classList.remove("show-modal");
 
     // desativa a li quando o modal fecha
-    itensLi.forEach((btn)=>{
+    itensLi.forEach((btn) => {
         btn.classList.remove("active");
-        liHome.classList.add("active")
-    })
-   
-    
+        liHome.classList.add("active");
+    });
 };
 
 // *******************************************************************************
@@ -64,11 +58,10 @@ const formaPagamento = document.getElementById("forma-pagamento");
 const botaoAdicionar = document.querySelector(".btn-add");
 const btnCancelar = document.querySelector(".btn-cancelar");
 
-let listaServicos = []
+let listaServicos = [];
 btnCancelar.addEventListener("click", () => {
     limpaCampo();
 });
-
 
 // Utilitário para formatar data (de ISO para BR)
 const formatarDataParaExibicao = (dataISO) => {
@@ -77,57 +70,64 @@ const formatarDataParaExibicao = (dataISO) => {
     return `${dia}/${mes}/${ano}`;
 };
 
-
 const mensagem = document.querySelector(".mensagem-sucesso");
 const campoSaldo = document.querySelector(".saldo");
 let saldoTotal = 0;
+
+const definirDataAtualComoPadrao = () => {
+    const hoje = new Date();
+    hoje.setMinutes(hoje.getMinutes() - hoje.getTimezoneOffset());
+    const dataFormatadaParaInput = hoje.toISOString().split("T")[0];
+    dataServico.value = dataFormatadaParaInput;
+};
+
+definirDataAtualComoPadrao();
+
 const criaCampoServico = () => {
     const tipoServico = servico.value.trim();
     const data = dataServico.value.trim();
     const valor = parseFloat(valorServico.value.trim());
     const formaPag = formaPagamento.value.trim();
 
-   
-
     novoServico = {
         id: new Date().getTime(),
         tipo: tipoServico,
         valor: valor,
         data: data,
-        forma: formaPag
-    }
+        forma: formaPag,
+    };
 
-    listaServicos.push(novoServico)
+    listaServicos.push(novoServico);
 
     saldoTotal += valor;
     campoSaldo.textContent = `Total ${saldoTotal.toLocaleString("pt-br", {
-            style: "currency",
-            currency: "BRL",
-        })}`;
+        style: "currency",
+        currency: "BRL",
+    })}`;
     mensagem.textContent = "Serviço cadastrado com sucesso";
     setTimeout(() => {
-            mensagem.textContent = "";
+        mensagem.textContent = "";
     }, 2000);
 
-    renderizaServico(novoServico)
- };
+    renderizaServico(novoServico);
+    definirDataAtualComoPadrao();
+};
 
-    let containerVendas = document.querySelector(".vendas");
+let containerVendas = document.querySelector(".vendas");
 
-
- const renderizaServico = (servico)=>{
+const renderizaServico = (servico) => {
     const dataFormatada = formatarDataParaExibicao(servico.data);
 
     containerVendas.innerHTML += `<div class="venda">
                         <p>${servico.tipo}</p>
                         <p>${dataFormatada}</p>
                          <p>${servico.valor.toLocaleString("pt-br", {
-            style: "currency",
-            currency: "BRL",
-        })}</p>
+                             style: "currency",
+                             currency: "BRL",
+                         })}</p>
                         <p class="forma-pag">${servico.forma}</p>
-                    </div>`
- }
+                    </div>`;
+};
 
 const limpaCampo = () => {
     servico.value = "";
